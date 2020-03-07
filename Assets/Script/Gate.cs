7f1afca1918da3af3b54045b65a4787cdf21eaba
Sunrise <sunrisewng@gmail.com>
2020-03-07 15:48:07 +0100

    moved some scripts around, gate prefab and shader

---
+++ Assets/Script/Gate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public float gate_open_time = 2f;
+
+    public float curr_gate_time_ = 2f;
+    private Material mat_;
+
+    public void ResetGate()
+    {
+        curr_gate_time_ = gate_open_time;
+    }
+
+    public void OpenGate(float val)
+    {
+        curr_gate_time_ -= val;
+        if(curr_gate_time_ <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void UpdateGateVisuals()
+    {
+        mat_.SetFloat("_GateStatus", curr_gate_time_ / gate_open_time);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat_ = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGateVisuals();
+    }
+}
